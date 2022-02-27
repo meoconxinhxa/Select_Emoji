e38f058cdd6b8d302e601fb1fd51b10af8bde96f
meoconxinhxa <suong2012001@gmail.com>
2022-02-28 00:55:22 +0700

    update code

---
+++ app/src/main/java/com/example/tsuongnt/IconService.java
@@ -0,0 +1,23 @@
+package com.example.tsuongnt;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class IconService extends ArrayAdapter {
+    public IconService(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
---
+++ app/src/main/java/com/example/tsuongnt/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.tsuongnt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+    String target;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView=findViewById(R.id.myGirdView);
+        List<Integer> emoji = Arrays.asList(
+                8986, 0x1F603, 0x1F605, 0x1F60D, 0x1F60F,
+                0x1F618, 0x1F621, 0x1F625, 0x1F628, 0x1F62D,
+                0x1F637, 0x1F61D, 0x1F616, 0x1F609, 0x1F60B,
+                0x1F635, 0x1F633, 0x1F624, 0x1F61C, 0x1F60A);
+        List data = new ArrayList();
+        for (int i = 0; i < emoji.size(); i++) {
+            data.add(new String(Character.toChars(emoji.get(i))));
+        }
+        IconService iconService = new IconService(getApplicationContext(), R.layout.icon_layout, data);
+        gridView.setAdapter(iconService);
+
+        List icons = new ArrayList(data);
+        target = RandomIcon(icons);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView v = (TextView) view;
+                String selected = v.getText().toString();
+                if(selected.equals(target)){
+                    icons.remove(target);
+                    v.setText("");
+                }
+                target = RandomIcon(icons);
+            }
+        });
+    }
+
+    private String RandomIcon(List icons) {
+        Random rand = new Random();
+        String result = icons.get(rand.nextInt(icons.size())).toString();
+        TextView target = findViewById(R.id.target);
+        target.setText(result);
+        return result;
+    }
+}
